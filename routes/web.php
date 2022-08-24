df0a56ff1721d49a13ff4f43d53fde79384edbef
Paisa2 <villca.fuentes23@gmail.com.com>
2022-08-24 19:01:19 -0400

    index.sexo

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SexoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('sexos.index');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/', [SexoController::class,'index']);
